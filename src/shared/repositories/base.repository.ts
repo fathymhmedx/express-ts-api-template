6537b5d9724d5fbdc653e147c69f84b315461aa2
Fathy Mohamed <fathymhmed11@gmail.com>
2025-12-31 07:08:39 +0200

    Add BaseRepository<T> generic class in shared/repositories for common CRUD operations

---
+++ src/shared/repositories/base.repository.ts
@@ -0,0 +1,27 @@
+export class BaseRepository<T> {
+  private model: any;
+
+  constructor(model: any) {
+    this.model = model;
+  }
+
+  async create(data: Partial<T>): Promise<T> {
+    return this.model.create(data);
+  }
+
+  async findAll(): Promise<T[]> {
+    return this.model.find();
+  }
+
+  async findOne(id: string): Promise<T | null> {
+    return this.model.findById(id);
+  }
+
+  async update(id: string, data: Partial<T>): Promise<T | null> {
+    return this.model.findByIdAndUpdate(id, data, { new: true });
+  }
+
+  async remove(id: string): Promise<T | null> {
+    return this.model.findByIdAndDelete(id);
+  }
+}
